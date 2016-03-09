672dda42a7b02eaebfd834a74498810c7d1a5e94
Ryan James Spencer <spencer.ryanjames@gmail.com>
2016-03-09 11:19:21 +1100

    Add test-refresh for lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
+{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                  [com.jakemccrary/lein-test-refresh "0.14.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
