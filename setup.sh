6d8d58047fd9e30f9416b58170a5d6c91251c437
Ryan Spencer <rspencer@freelancer.com>
2017-03-23 13:11:55 +1100

    Update gitconfig from work

--- setup.sh
+++ setup.sh
@@ -5,10 +5,13 @@
 # summary: Symlinks files in users root directory the files living here.
 
 backup_dir="$HOME/.dotfiles.bk"
-config_files=$(find -type f | grep -Zv .git);
+config_files=$(find -type f | grep -v .git);
 
-for file in "$config_files"; do
-  if [ "$file" = "$0" ]; then echo "$file"; fi
+for file in $config_files; do
+  [ $file = $0 ] && continue
+
+  diff "$file" "~/$file"
+
   # Skip any ignored files.
   #filename=$(basename $file)
   #if [[ -n "${ignored_files[$filename]}" ]]; then
