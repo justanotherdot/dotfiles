05e2036fbe4284868ebbaaf252dce8e8a31ffc45
Ryan Spencer <rspencer@freelancer.com>
2017-03-31 08:50:16 +1100

    Add rudimentary setup script

--- setup.sh
+++ setup.sh
@@ -1,38 +1,20 @@
 #!/bin/bash
-#
-# file: setup.sh
 # author: Ryan James Spencer
-# summary: Symlinks files in users root directory the files living here.
 
-backup_dir="$HOME/.dotfiles.bk"
-config_files=$(find -type f | grep -v .git);
+set -eu
 
-for file in $config_files; do
-  [ $file = $0 ] && continue
+backup_dir=".dotfiles.bk"
+dotfiles=$(find -type f | egrep -v "(.git/|$0)" | sed 's/\.\///')
 
-  diff "$file" "~/$file"
+mkdir -p "$HOME/$backup_dir"
 
-  # Skip any ignored files.
-  #filename=$(basename $file)
-  #if [[ -n "${ignored_files[$filename]}" ]]; then
-    #continue
-  #fi
-  #echo -e "$file\n"
-
-  #if [[ -e "$HOME/$file" ]]; then
-    ## Backup the old config files someplace safe.
-    #mkdir -p "$backup_dir"
-    #cp -r "$file" "$backup_dir"
-    #echo "Copying $file to $backup_dir"
-
-    ## Do tell if there are differences between the files.
-    #diff -q "$HOME/$file" "$PWD/$file" &>/dev/null
-    #if [ $? -eq 1 ]; then
-      #echo "$file is different from local copy!"
-    #fi
-  #fi
-
-  #echo "Linking $HOME/$file to $PWD/$file"
-  ## TODO remove verbose flag.
-  ##ln -sfv "$HOME/$file" "$PWD/$file"
+for dotfile in $dotfiles; do
+  # Backup all the respective files.
+  # TODO Want a better way to do multiple backups.
+  if [[ -f "$HOME/$dotfile" ]]; then
+    cp -vf "$HOME/$dotfile" "$HOME/$backup_dir/$dotfile"
+  fi
+  # TODO Improve this with an env-var that designates the dotfile git repo.
+  # Symlink dotfile in home directory to this dotfile directory.
+  ln -vsf "$PWD/$dotfile" "$HOME/$dotfile"
 done
